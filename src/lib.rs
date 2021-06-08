ba696c5eb154bb14aa8c5636da704684145dcb5b
Olybear9 <olybear9@gmail.com>
2021-06-07 23:25:20 -0500

    🧪 test: Update test

--- src/lib.rs
+++ src/lib.rs
@@ -2,20 +2,3 @@
 pub mod compiler;
 pub mod error;
 //pub mod parser_h::{ parser, compile };
-
-fn main() {
-	let code = "use std.math;
-	/*const mychild: string = 10;
-	const newval = (int) mychild;
-	debug!(newval); // Number<u8, 10>
-	*/";
-	let lexed = compiler::typesharp_lexer::tokenize(&code);
-	let val = lexed.len();
-
-	for token in lexed {
-		println!("Found: {:?}", token);
-	}
-
-	println!("There are: {} tokens.", val);
-	//compile(parser::parse(lexed));
-}
--- test/src/main.rs
+++ test/src/main.rs
@@ -1,3 +1,30 @@
+use crate::{ typesharp::* };
+use crate::{ typesharp::compiler::typesharp_ast_old::Features };
+use std::fs::write;
 fn main() {
-    println!("Hello, world!");
+	let code = "use std.math;
+	/*const mychild: string = 10;
+	const newval = (int) mychild;
+	debug!(newval); // Number<u8, 10>
+	*/";
+	let lexed = compiler::typesharp_lexer::tokenize(&code);
+	let val = lexed.len();
+
+	for token in lexed {
+		println!("Found: {:?}", token);
+	}
+
+	println!("There are: {} tokens.", val);
+
+	let bin = compile(code, compiler::typesharp_ast_two::ASTOpts {
+		assembler: compiler::typesharp_ast_two::Assembler::LLVM,
+		mangle: false,
+		library: false,
+		entry: "main",
+		features: Features::Defaults,
+		imports: vec!()
+		debug: true
+	});
+
+	write("test.exe", bin.to_bytes());
 }
