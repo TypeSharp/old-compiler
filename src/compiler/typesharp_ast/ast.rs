d50c7c6bd461e3ee5bb114b1bacf3bb6d2517974
Olybear9 <olybear9@gmail.com>
2021-06-07 21:53:38 -0500

    📝 chore: Start working on AST

--- src/compiler/typesharp_ast/ast.rs
+++ src/compiler/typesharp_ast/ast.rs
@@ -1,6 +1,8 @@
 // Typesharp ast
 // use crate::{ typesharp_parser::Module };
 use super::position::{ Position };
+use super::op::*;
+use crate::{ compiler::typesharp_lexer::token::Token };
 
 pub struct AST {
 	// name
@@ -13,9 +15,17 @@ pub struct AST {
 	// b: Vec<ASTNode>
 }
 
-pub trait AnyContext {
-	fn new(&self) -> Self;
-	fn getLine() -> u8;
+#[derive(Clone, PartialEq, Debug)]
+pub struct AnyContext;
+
+impl AnyContext {
+	fn new(&self) -> AnyContext {
+		return Self;
+	}
+
+	fn getLine() -> u8 {
+		0
+	}
 }
 
 pub enum ProgramType {
@@ -33,13 +43,57 @@ pub enum ProgramType {
 
 pub struct Library;
 
-pub struct Statement<Context> {
-	pub body: StmtBody,
-	pub context: Context,
+#[derive(Clone, PartialEq, Debug)]
+pub struct ASTStatement {
+	pub body: ASTStateBody,
+	pub context: AnyContext,
 	pub pos: Position
 }
 
 #[derive(Clone, PartialEq, Debug)]
-pub enum StmtBody {
+pub enum ASTStateBody {
 	// expressions, function calls, returns etc should be here.
+	FuncCall(Signature)
+}
+
+// Context and definitions
+/// A variable, const, class, etc.
+#[derive(Clone, PartialEq, Debug)]
+pub struct Var {
+	pub op: Option<AnyOp>,
+	pub val: Token,
+	pub pos: Position,
+	pub dies: bool,
+	// pub typ: Type
+}
+
+/// Dynamic variable, extends var, which is static.
+#[derive(Clone, PartialEq, Debug)]
+pub struct HeapVar {
+	pub var: Var,
+	pub mangled: bool
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub enum AnyVar {
+	Static(Var),
+	Heap(HeapVar)
+}
+
+/// Functions
+#[derive(Clone, PartialEq, Debug)]
+pub struct Signature {
+	pub name: Var,
+	pub dynamic: bool
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct Expression {
+	pub ops: Vec<AnyOp>,
+	pub v: Vec<AnyVar>
+}
+
+pub struct Conditional {
+	pub condition: Expression,
+	pub body: Vec<ASTStatement>
 }
--- src/compiler/typesharp_ast/types/mod.rs
+++ src/compiler/typesharp_ast/types/mod.rs
@@ -1,1 +1,16 @@
-/// Types module
+// This is a module that handles internal classes and types.
+#[derive(Clone, PartialEq, Debug)]
+pub struct Type {
+	pub name: String,
+	pub statement: TypeDefinition
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct TypeDefinition {
+	pub func: TypeSignature
+}
+
+#[derive(Clone, PartialEq, Debug)]
+pub struct TypeSignature {
+	
+}
