e58a31134d1d934031681803bbf2106fa205e942
sal1ent <38633228+sal1ent@users.noreply.github.com>
2021-06-09 12:14:12 -0700

    Update error.rs
    * Change fatal to unreachable, for better compiler diagnostics
    * Fix weird spelling errors
    * to_str matches

--- src/error/typesharp_error/error.rs
+++ src/error/typesharp_error/error.rs
@@ -6,7 +6,6 @@ use crate::{
 	}
 };
 
-
 /// A enum that is a representation of error types.
 /// The errors represented here are errors that are related to 
 /// The compilers syntax-checking.
@@ -15,14 +14,15 @@ use crate::{
 /// that are related to that feature.
 /// 
 /// An example would be: IO::ErrUnsupported, IO::
-pub enum ErrGeneral {
-	// An error.
+pub enum ErrCompiler {
+	/// An error. (general error)
 	Error,
 	
-	// A fatal error (for possible memory related issues).
-	Fatal,
+	/// A fatal error (for possible memory related issues, or maybe even compiler issues that
+     /// the user wasn't supposed to reach).
+	Unreachable,
 
-	// A warning.
+	/// A warning.
 	Warning,
 
 	/// A suggestion.
@@ -37,22 +37,21 @@ pub enum ErrGeneral {
 /// 
 /// Note: These are not related to IO, or any other feature in the stdlib.
 pub enum ErrFeature {
-	// All went well, success.
+	/// All went well, success.
 	Success,
 
-	// Invalid handle that was trying to be accessed.
+	/// Invalid handle that was trying to be accessed.
 	InvalidHandle,
 
-	// Not enough memory allocated.
+	/// Not enough memory allocated.
 	NotEnoughMemory,
 
-	// Unable to Write to a certain destination.
+	/// Unable to Write to a certain destination.
 	WriteFault,
 
-	// Unable to Read from certain destination.
+	/// Unable to Read from certain destination.
 	ReadFault,
 }
-
 
 /// Trait that all compiler-level errors adhere to.
 trait ErrImpl {
@@ -60,14 +59,14 @@ trait ErrImpl {
 }
 
 
-impl ErrImpl for ErrGeneral {
+impl ErrImpl for ErrCompiler {
 	fn to_str(&self) -> &'static str {
 		match *self {
-			ErrGeneral::Error => "Syntax Error",
-			ErrGeneral::Fatal => "Fatal Error",
-			ErrGeneral::Note => "Note",
-			ErrGeneral::Suggestion => "Suggestion:",
-			ErrGeneral::Warning => "Warning",
+			ErrCompiler::Error => "Compiler Error",
+			ErrCompiler::Unreachable => "Unreachable point accessed in the compiler, this is an internal compiler issue.",
+			ErrCompiler::Note => "Note:",
+			ErrCompiler::Suggestion => "Suggestion:",
+			ErrCompiler::Warning => "Warning:",
 		}
 	}
 }
@@ -77,7 +76,7 @@ impl ErrImpl for ErrFeature {
 		match *self {
 		     ErrFeature::Success => "Sucess",
 			ErrFeature::InvalidHandle => "Invalid Handle being accessed",
-			ErrFeature::NotEnoughMemory => "Note enough memory available",
+			ErrFeature::NotEnoughMemory => "Not enough memory available",
 			ErrFeature::WriteFault => "Unable to write to certain destination.",
 			ErrFeature::ReadFault => "Unable to read from certain desintation.",
 		}
